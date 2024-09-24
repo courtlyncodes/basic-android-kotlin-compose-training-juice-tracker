bddbc0f2e0dc9cab241bddccc894692da0ea4998
Courtlyn Boykin <courtlynb@google.com>
2024-09-24 15:01:10 -0400

    Complete the fragment

---
+++ app/src/main/java/com/example/juicetracker/EntryDialogFragment.kt
@@ -0,0 +1,44 @@
+package com.example.juicetracker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.example.juicetracker.data.JuiceColor
+import com.example.juicetracker.databinding.FragmentEntryDialogBinding
+import com.example.juicetracker.ui.AppViewModelProvider
+import com.example.juicetracker.ui.EntryViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class EntryDialogFragment : BottomSheetDialogFragment() {
+    val entryViewModel: EntryViewModel by viewModels<EntryViewModel> { AppViewModelProvider.Factory }
+    var selectedColor: JuiceColor = JuiceColor.Red
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentEntryDialogBinding.inflate(inflater, container, false).root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = FragmentEntryDialogBinding.bind(view)
+        val args: EntryDialogFragmentArgs by navArgs()
+        val juiceId = args.itemId
+
+        binding.saveButton.setOnClickListener {
+            entryViewModel.saveJuice(
+                juiceId,
+                binding.name.text.toString(),
+                binding.description.text.toString(),
+                selectedColor.name,
+                binding.ratingBar.rating.toInt()
+            )
+            dismiss()
+        }
+
+    }
+}
